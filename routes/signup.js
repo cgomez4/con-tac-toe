39368ea018ca3411260b223c75d57d8f1f8adac5
slde12 <xuehui.chen1994@gmail.com>
2018-08-31 21:13:43 -0400

    front end and beck end connected

---
+++ routes/signup.js
@@ -0,0 +1,10 @@
+//signup page
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('signup');
+});
+
+module.exports = router;
