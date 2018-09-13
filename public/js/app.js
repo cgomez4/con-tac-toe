6ae1fec06dcfb7d7f0fe238377e05c1560e575e1
Freddy Haug <haug.freddy01@gmail.com>
2018-09-13 02:13:17 -0400

    Add contact and render contacts functional

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ function push() {
 //This is for adding new contacts, Evetually we need these variables to pass over to the backend to save the contact to database
 //change id from he and he1 to a unique id that that will delete any element on page with that id.
 function pushNewContact(name, number, email, address, id) {
-  $(".collapsible").append("<li><div id='"dfghjk"' class='collapsible-header black-text text'><i class='material-icons'>account_circle</i>"+id+"</div><div id='he1' class='collapsible-body white black-text text' row><div class='valign-wrapper'><i class='material-icons'>call</i><h6 class='col'>"+number+"</h6><i class='material-icons'>email</i><h6 class='col'>"+email+"</h6><i class='material-icons'>place</i><h6 class='col'>"+address+"</h6></div><a row s1 class='btn red' onclick='event.stopPropagation(); pushDeleteContact()'><i col offset-s10 class='material-icons'>delete</i></a></li>");
+  $(".collapsible").append("<li><div id='"+id+"' class='collapsible-header black-text text'><i class='material-icons'>account_circle</i>"+name+"</div><div id='"+id+"' class='collapsible-body white black-text text' row><div class='valign-wrapper'><i class='material-icons'>call</i><h6 class='col'>"+number+"</h6><i class='material-icons'>email</i><h6 class='col'>"+email+"</h6><i class='material-icons'>place</i><h6 class='col'>"+address+"</h6></div><a row s1 class='btn red' onclick='event.stopPropagation(); pushDeleteContact()'><i col offset-s10 class='material-icons'>delete</i></a></li>");
   //COLLAPSIBLE INITIALIZATION
  $('.collapsible').collapsible();
 }
@@ -29,7 +29,6 @@ function pushDeleteContact() {
 }
 
 function pushLogOut(sample) {
-  console.log("youre out");
   sessionStorage.clear();
 
 }
